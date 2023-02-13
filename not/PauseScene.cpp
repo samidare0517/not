f6f47ef6bd787ac4d34bf5369544089f940863c4
石橋 叶恵 <kanaesachiho@gamil.com>
2023-02-13 11:57:16 +0900

    ポーズシーンを変更

--- not/PauseScene.cpp
+++ not/PauseScene.cpp
@@ -23,27 +23,27 @@ void PauseScene::Update(const InputState& input)
 
 void PauseScene::Draw()
 {
-	constexpr int pw_width   = 400;	// ポーズ枠の幅
-	constexpr int pw_height  = 400;	// ポーズ枠の高さ
-	constexpr int pw_start_x = (900 - pw_width)  / 2;	// ポーズ枠左
+	constexpr int pw_width = 500;	// ポーズ枠の幅
+	constexpr int pw_height = 500;	// ポーズ枠の高さ
+	constexpr int pw_start_x = (1600 - pw_width) / 2;	// ポーズ枠左
 	constexpr int pw_start_y = (900 - pw_height) / 2;	// ポーズ枠上
 
 //	SetDrawBlendMode(DX_BLENDMODE_MULA, 128);	// 乗算合成
 
 	// ポーズウィンドウセロファン(黒)
-	DrawBox(pw_start_x,  pw_start_y,
-			pw_start_x + pw_width,
-			pw_start_y + pw_height,
-			GetColor(188, 221, 255), true);
+	DrawBox(pw_start_x, pw_start_y,
+		pw_start_x + pw_width,
+		pw_start_y + pw_height,
+		GetColor(188, 221, 255), true);
 
 	// 通常描画に戻す
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
 	// ポーズウィンドウ枠線
-	DrawBox(pw_start_x,  pw_start_y,
-			pw_start_x + pw_width,
-			pw_start_y + pw_height,
-			0xffffff, false);
+	DrawBox(pw_start_x, pw_start_y,
+		pw_start_x + pw_width,
+		pw_start_y + pw_height,
+		0xffffff, false);
 
 	// ポーズ中メッセージ
 	SetFontSize(50);
