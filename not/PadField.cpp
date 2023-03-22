deb498151b1c87ae8fb25e0c6a96f5c0c6ab7841
石橋 叶恵 <kanaesachiho@gamil.com>
2023-03-22 15:16:02 +0900

    変更

--- not/PadField.cpp
+++ not/PadField.cpp
@@ -95,7 +95,7 @@ void PadField::NormalUpdate(const InputState& input)
 
 	// 正解数が30になったらクリア画面へ
 	SetFontSize(50);
-	if (answerNum == 1)
+	if (answerNum == 20)
 	{
 		answerCheck = false;	// 正解のフラグの初期化
 
